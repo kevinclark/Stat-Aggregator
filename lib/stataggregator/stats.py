ef98fe8fe8490fd16e667f48bd61d71d0352fbbe
Robby Walker <robby.walker@gmail.com>
2010-10-08 22:17:20 -0700

    Connect stats to stat aggregator

--- lib/stataggregator/stats.py
+++ lib/stataggregator/stats.py
@@ -17,28 +17,71 @@
 #  limitations under the License.
 #
 
-from twisted.internet import task
+from twisted.application import internet
+from twisted.internet import protocol, task
+from twisted.protocols import basic
 
 import simplejson as json
 
-stats = {}
+
+__statTracker = None
 
 
 def init():
-	result = task.LoopingCall(__flush)
-	result.start(1, False)
-	return result
+	global __statTracker
+	__statTracker = StatTracker()
 
 
-def incrementStat(name):
-	stats[name] = stats.get(name, 0) + 1
+def incrementStat(self, name):
+	global __statTracker
+	__statTracker.incrementStat(name)
 
 
-def __getStatsToSend():
-	global stats
-	result, stats = stats, {}
-	return result
+
+class StatTracker(object):
+	stats = {}
+
+	def __init__(self):
+		flushLoop = task.LoopingCall(self.__flush)
+		flushLoop.start(1, False)
+		
+		self.aggregator = StatReceiverClientFactory()
+		from twisted.internet import reactor
+		reactor.connectTCP('localhost', 1234, self.aggregator)
 
 
-def __flush():
-	print json.dumps(__getStatsToSend())
+	def incrementStat(self, name):
+		stats[name] = stats.get(name, 0) + 1
+
+
+	def __getStatsToSend(self):
+		result, self.stats = self.stats, {}
+		return result
+
+
+	def __flush(self):
+		out = json.dumps(self.__getStatsToSend())
+		if self.aggregator.instance:
+			self.aggregator.instance.sendLine(out)
+		print out
+
+
+
+class StatReceiverClient(basic.LineReceiver):
+	"""Protocol for a stat flush client."""
+
+	factory = None
+
+	def connectionMade(self):
+		self.factory.instance = self
+
+	def connectionLost(self, _):
+		"""Logs the connection failure and closes the incoming connection."""
+		log.err("Lost connection to the stat receiver")
+
+
+
+class StatReceiverClientFactory(protocol.ClientFactory):
+	"""Factory for a stat flush client."""
+	protocol = StatReceiverClient
+	instance = None
